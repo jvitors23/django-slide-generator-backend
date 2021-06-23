466e8475c05526047c31453c4cb7555e5396fa66
José Vitor Santos Silva <jose.silva@dcomp.ufs.br>
2021-06-22 21:15:41 -0300

    Create serializer for slide attributes request object

--- app/slides_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SlidesApiConfig(AppConfig):
-    name = 'slides_api'
--- app/slides_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'user'
-
-urlpatterns = [
-    path('slide-data/', views.SlidesAPIView.as_view()),
-]
