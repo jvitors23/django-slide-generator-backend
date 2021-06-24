879de0d2ac8cca1469912923c6ad230e417821bc
José Vitor Santos Silva <jose.silva@dcomp.ufs.br>
2021-06-24 18:17:03 -0300

    Added help texts for the slide api params

--- src/slides_api/serializers.py
+++ src/slides_api/serializers.py
@@ -3,6 +3,13 @@ from rest_framework import serializers
 
 class SlideFeaturesSerializer(serializers.Serializer):
     """Serializer for slide attributes"""
-    max_slides = serializers.IntegerField(min_value=5, max_value=30)
-    subject = serializers.CharField(max_length=255)
-    language = serializers.ChoiceField(choices=['en', 'pt'])
+    max_slides = serializers.IntegerField(min_value=5, max_value=30,
+                                          help_text='Max number of slides '
+                                                    'considering '
+                                                    'introduction and '
+                                                    'references')
+    subject = serializers.CharField(max_length=255, help_text='Slide subject')
+    language = serializers.ChoiceField(choices=['en', 'pt'],
+                                       help_text="Slide language, current "
+                                                 "supported languages: [en, "
+                                                 "pt]")
